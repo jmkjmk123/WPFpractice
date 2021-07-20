0485d6e615816f590a1f9b8b162edd428d8c702b
jmkjmk123 <icejmk123@gmail.com>
2021-07-20 15:55:37 +0900

    radio button 연습 1
    radio button 연습 1

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void arg2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Hello World");
+
+            int a= Convert.ToInt32(arg1.Text);
+            int b = Convert.ToInt32(arg2.Text);
+        }
+
+        private void arg1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
